a3dc4f1581cec2fe1e2dcf281d38694cd5b43bc2
williamfi76 <wfrederickiv76@gmail.com>
2023-10-14 08:44:51 +0700

    Added Stuff

--- SIA/src/App.java
+++ SIA/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, William");
+        System.out.println("I'm good at this");
+        //Added Comments
     }
 }
